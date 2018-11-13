606ed0b81d1cebdf4e05d19f809aefe0a34e53e1
jimmyVerdasca <jimmy.verdasca@heig-vd.ch>
2018-11-13 12:57:48 +0100

    servlet Users implementation

--- src/main/java/ch/heigvd/amt/mvc/web/controllers/UsersServlet.java
+++ src/main/java/ch/heigvd/amt/mvc/web/controllers/UsersServlet.java
@@ -1,5 +1,9 @@
 package ch.heigvd.amt.mvc.web.controllers;
 
+import ch.heigvd.amt.jdbc.dao.UsersManager;
+import ch.heigvd.amt.jdbc.model.User;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,11 +13,21 @@ import java.io.IOException;
 
 @WebServlet(name = "UsersServlet", urlPatterns = {"/users"})
 public class UsersServlet extends HttpServlet {
+
+    @EJB
+    UsersManager userManager;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        User user = (User)request.getSession().getAttribute("user");
+        if(!userManager.isAdmin(user)) {
+            response.setStatus(403);
+        } else {
+            request.setAttribute("users", userManager.getUsers());
+        }
+        request.getRequestDispatcher("/WEB-INF/pages/users.jsp").forward(request, response);
     }
 }
