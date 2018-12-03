c8a36ebb7cb8e5a905a352ea3745e099db7614c8
Mika Pagani <mika.pagani@heig-vd.ch>
2018-12-03 17:16:23 +0100

    Added PointScale

---
+++ Part2/swagger/spring-server/src/main/java/io/avalia/badges/entities/PointScaleEntity.java
@@ -0,0 +1,38 @@
+package io.avalia.badges.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class PointScaleEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private Integer value;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
---
+++ Part2/swagger/spring-server/src/main/java/io/avalia/badges/repositories/PointScaleRepository.java
@@ -0,0 +1,7 @@
+package io.avalia.badges.repositories;
+
+import io.avalia.badges.entities.PointScaleEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PointScaleRepository extends CrudRepository<PointScaleEntity, Long> {
+}
